85ce8709d37319d981fc2b3ec982bbb324e1b6aa
pavelgerasimchik20 <pavelgerasimchik20@gmail.com>
2025-09-16 14:39:09 +0300

    styles changes

--- message-app/src/App.tsx
+++ message-app/src/App.tsx
@@ -1,14 +1,24 @@
-
-import { useState } from 'react';
-import Welcome from './pages/Welcome';
-import SendForm from './pages/SendForm';
+import { useState } from "react";
+import Welcome from "./pages/Welcome";
+import SendForm from "./pages/SendForm";
 
 export default function App() {
-  const [step, setStep] = useState<'welcome'|'form'>('welcome');
+  const [step, setStep] = useState<"welcome" | "form">("welcome");
   return (
-    <div style={{ maxWidth: 720, margin: 40 }}>
-      {step === 'welcome' ? <Welcome onNext={() => setStep('form')} /> : <SendForm onBack={() => setStep('welcome')} />}
+    <div
+      style={{
+        width: "100vw",
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      {step === "welcome" ? (
+        <Welcome onNext={() => setStep("form")} />
+      ) : (
+        <SendForm onBack={() => setStep("welcome")} />
+      )}
     </div>
   );
 }
-
--- message-app/src/pages/Welcome.tsx
+++ message-app/src/pages/Welcome.tsx
@@ -2,10 +2,20 @@ export default function Welcome({ onNext }: { onNext: () => void }) {
   return (
     <div>
       <h1>Здравствуйте!</h1>
-      <p>
-        Добро пожаловать! Нажмите кнопку ниже, чтобы перейти к форме и отправить сообщение.
+      <p style={{
+        fontSize: ""
+      }}>
+        Добро пожаловать! Нажмите кнопку ниже, чтобы перейти к форме и отправить
+        сообщение.
       </p>
-      <button onClick={onNext}>Далее</button>
+      <button
+        style={{
+          marginTop: "2vh",
+        }}
+        onClick={onNext}
+      >
+        Далее
+      </button>
     </div>
   );
 }
